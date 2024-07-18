d94355830f636b880dd3c5397d602dee818b3e14
Blissmal <malutibethuel@gmail.com>
2024-07-18 21:34:03 +0300

    update

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,22 +1,49 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "./UserReducer";
 
 const Create = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const users = useSelector((state) => state.users);
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addUser({ id: users[users.length - 1].id + 1, name, email }));
+  };
+
   return (
-    <div className='d-flex w-100 vh-100 justify-content-center align-items-center'>
+    <div className="d-flex w-100 vh-100 justify-content-center align-items-center">
       <div className="w-50 border bg-secondary text-white p-5">
-        <form>
-            <div>
-                <label htmlFor="name">Name: </label>
-                <input type="text" name='name' className='form-control'/>
-            </div>
-            <div>
-                <label htmlFor="email">Email: </label>
-                <input type="email" name='name' className='form-control'/>
-            </div>
+        <h3>Add New User</h3>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="name">Name: </label>
+            <input
+              type="text"
+              name="name"
+              className="form-control"
+              placeholder="enter name"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email: </label>
+            <input
+              type="email"
+              name="email"
+              className="form-control"
+              placeholder="enter email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <br />
+          <button className="btn btn-info">Submit</button>
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Create
+export default Create;
