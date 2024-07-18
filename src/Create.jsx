efa81cd6daed7a62232ffd6a5939bb7389d53fb5
Blissmal <malutibethuel@gmail.com>
2024-07-18 20:44:38 +0300

    updated

--- src/Create.jsx
+++ src/Create.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 const Create = () => {
   return (
-    <div>
-      
+    <div className='d-flex w-100 vh-100 justify-content-center align-items-center'>
+      <div className="w-50 border bg-secondary text-white p-5">
+        <form>
+            <div>
+                <label htmlFor="name">Name: </label>
+                <input type="text" name='name' className='form-control'/>
+            </div>
+            <div>
+                <label htmlFor="email">Email: </label>
+                <input type="email" name='name' className='form-control'/>
+            </div>
+        </form>
+      </div>
     </div>
   )
 }
